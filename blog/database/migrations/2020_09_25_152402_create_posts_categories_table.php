65f250778695d8c22d94fb523c5ffc5aa9c38014
Wesley Flôres <wesleyfloresterres@gmail.com>
2020-09-25 13:07:28 -0300

    migrations

--- blog/database/migrations/2020_09_25_152402_create_posts_categories_table.php
+++ blog/database/migrations/2020_09_25_152402_create_posts_categories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreatePostsCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('posts_categories', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
             $table->softDeletes();
+            $table->unsignedBigInteger('post_id');
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('post_id')->references('id')->on('posts');
+            $table->foreign('category_id')->references('id')->on('categories');
         });
     }
 
@@ -32,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('posts_categories');
     }
 }
